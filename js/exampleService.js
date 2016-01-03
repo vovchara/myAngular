287d05469d7a4810c691807a39e457ff6e01f512
vmartyniuk <vmartyniuk@VMartyniuk.playtika.local>
2016-01-03 20:07:42 +0200

    unique value checking moved inside service\factory

--- js/exampleService.js
+++ js/exampleService.js
@@ -1,26 +1,38 @@
 (function () {
     "use strict";
     var app = angular.module("app");
+
     app.factory("exampleFactory", function () {
         console.log("factory init");
         var languages = ["AS3", "C#", "JAVA", "JS"];
         return {
-            addLang : function (lang){languages.push(lang)},
-            getLanguages : function (){return languages},
-            hasLang : function (lang){return languages.indexOf(lang) > -1}
+            addLang: function (lang) {
+                if (languages.indexOf(lang) == -1) {
+                    languages.push(lang);
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            getLanguages: function () {
+                return languages
+            }
         }
     });
 
     app.service("exampleService", function () {
         var listOfCars = ["honda", "toyota", "bmw", "nissan", "mazda"];
         this.addCar = function (carName) {
-            listOfCars.push(carName);
+            if (listOfCars.indexOf(carName) == -1) {
+                listOfCars.push(carName);
+                return true;
+            } else {
+                return false;
+            }
+
         };
-        this.getCars = function (){
+        this.getCars = function () {
             return listOfCars;
         };
-        this.hasCar = function(carName){
-            return listOfCars.indexOf(carName) > -1;
-        }
     });
 })();
--- js/main.controller.js
+++ js/main.controller.js
@@ -1,30 +1,28 @@
-(function (){
+(function () {
     "use strict";
-    function MainController ($scope, exFact, exService) {
+    function MainController($scope, exFact, exService) {
         $scope.name = "user";
         $scope.show = true;
-        $scope.data = [1,2,3,4,5,6,7];
+        $scope.data = [1, 2, 3, 4, 5, 6, 7];
         $scope.click = function () {
             console.log($scope.name);
         };
 
         $scope.cars = exService.getCars();
-        $scope.addCar = function (newCar){
-            if (exService.hasCar(newCar)){
+        $scope.addCar = function (newCar) {
+            var successAdded = exService.addCar(newCar);
+            if (!successAdded) {
                 alert("Already exist!");
-            }else{
-                exService.addCar(newCar);
             }
         };
 
         $scope.languages = exFact.getLanguages();
-        $scope.addLang = function (lang){
-            if (exFact.hasLang(lang)){
+        $scope.addLang = function (lang) {
+            var successAdded = exFact.addLang(lang);
+            if (!successAdded) {
                 alert("Already know! ha-ha");
-            }else{
-                exFact.addLang(lang);
             }
-        }
+        };
     }
 
     angular.module("app").controller("MainController", ["$scope", "exampleFactory", "exampleService", MainController]);
